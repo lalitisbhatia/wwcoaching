5cc0df93160c4fbc15d23c812011c7b371c10b2d
Lalit Bhatia <lalitisbhatia@yahoo.com>
2014-05-10 21:39:36 -0400

    updates for updating notes in db

--- routes/api.js
+++ routes/api.js
@@ -187,7 +187,7 @@ exports.updateCallNote = function(req, res) {helper.getConnection(function(err,d
     console.log(note);
     
     db.collection(usersCollName, function(err, collection) {
-        collection.update({'_id':note.userid,CallNotes:{callid:note.callid}},{$set:{"date":note.date,"duration":note.duration,"note":note.note}}, function(err, item) {
+        collection.update({'_id':note.userid},{CallNotes:{$elemMatch:{callid:note.callid}}},{$set:{"date":note.date,"duration":note.duration,"note":note.note}}, function(err, item) {
             //console.log(item);
             res.send(item);
             //res.render('user',item);
