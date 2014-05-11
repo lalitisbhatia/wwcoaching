97f304702f3ba8c122f5237970811a46c5873ff9
Lalit Bhatia <lalitisbhatia@yahoo.com>
2014-05-10 20:03:23 -0400

    updates for updating notes in db

--- routes/api.js
+++ routes/api.js
@@ -185,7 +185,9 @@ exports.getCallNotes = function(req, res) {helper.getConnection(function(err,db)
 exports.updateCallNote = function(req, res) {helper.getConnection(function(err,db){
     var note = req.body;
     console.log(note);
-    // db.collection(usersCollName, function(err, collection) {
+     db.collection(usersCollName, function(err, collection) {
+         var item = db.usersCollName.findOne({'_id':note.userid,CallNotes:{callid:note.callid}});
+         res.send(item);
     //     collection.update({$and:[{'_id':note.userid},{"CallNotes":{"callid":note.callid}}]},{"date":note.date,"duration":note.duration,"note":note.note}, { upsert: true }, function(err, result) {
     //         if (err) {
     //             res.send({'error':'error occurred while saving the call note'});
@@ -194,10 +196,10 @@ exports.updateCallNote = function(req, res) {helper.getConnection(function(err,d
     //             res.send('successful update - ' + result);
     //         }
     //     });
-    // });
+     });
     
-    var item = db.usersCollName.findOne({'_id':note.userid,CallNotes:{callid:note.callid}});
-     return item;
+    
+    
 });
 };
 
