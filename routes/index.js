193a17ea1932313c76fc29ee42dff210e2604e60
lalit <lalitisbhatia@yahoo.com>
2014-05-20 07:56:52 -0400

    started handling default rout '/'

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,33 @@
 
 var coaches = require('../routes/coaches');
 
+exports.home = function(req, res,next){
+    console.log('arriving at admin user');
+    if(req.session.auth){
+        console.log('user is authorised');
+
+        if(req.session.isAdmin){
+            console.log('user is ADMIN');
+            console.log('redirecting to admin home');
+            res.render('admin');
+        }else if(req.session.isCoach){
+            console.log('user is COACH');
+            res.render('coach');
+        }else if(req.session.isParticipant){
+            console.log('user is PARTICIPANT');
+            res.render('participant');
+        }else{
+            console.log('defaulting to COACH');
+            res.render('coach');
+        }
+//        else {
+//            console.log('redirecting to coach home');
+//            res.render('coach')
+//        }
+    }else{
+        res.render('adminLogin');
+    }
+};
 
 exports.admin = function(req, res,next){
     console.log('arriving at admin user');
