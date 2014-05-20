094bd6d1cccc42161e240a58bd762260d648a8c8
Lalit Bhatia <lalit.bhatia@weightwatchers.com>
2014-05-20 18:30:56 -0400

    started testing services for admin controllers to use them properly

--- public/js/testAdminController.js
+++ public/js/testAdminController.js
@@ -1,11 +1,19 @@
 'use strict';
 
-adminModule.controller('AllUsersController',  function AllCoachesController($scope,$http,$log,testAdminService) {
+adminModule.controller('testAllUsersController',  function AllCoachesController($scope,$http,$log,testAdminService) {
     $scope.initApp=function() {
         $scope.coaches=[];
+        $scope.users=[];
         testAdminService.getAllUsers().then(function(data){
-            $scope.coaches = data;
+            $scope.users = data;
+            console.log($scope.users);
         });
+
+        testAdminService.getAllCoaches().then(function(data){
+            $scope.coaches = data;
+            console.log($scope.coaches);
+        });
+
     };
 
 });
--- public/js/testAdminService.js
+++ public/js/testAdminService.js
@@ -1,28 +1,46 @@
 'use strict';
-adminModule.factory('adminService',function($http,$log,$q,$rootScope){
+adminModule.factory('testAdminService',function($http,$log,$q,$rootScope){
     var myService = {
         getAllUsers: function () {
-            if (!promise) {
-                promise = $http({
+            return $http({
                     method: 'GET',
                     url: '/users'
                 })
                     .success(function (data) {
-                        $log.info("Successfully retrieved all users.");
+                        $log.info("Successfully retrieved all users for a coach.");
                     })
                     .error(function (status, headers, config) {
                         $log.log('failed to users: ' + status);
                     })
                     .then(function(response) {
                         var results = [];
-                        results=response.data.d;
-                        console.log(results);
+                        results=response.data;
+                        //console.log(results);
                         return results;
                     });
 
-            } // closing brace for if(!promise)
+            } ,
+        getAllCoaches: function () {
+            return $http({
+                method: 'GET',
+                url: '/coaches'
+            })
+                .success(function (data) {
+                    $log.info("Successfully retrieved all coaches.");
+                })
+                .error(function (status, headers, config) {
+                    $log.log('failed to coaches: ' + status);
+                })
+                .then(function(response) {
+                    var results = [];
+                    results=response.data;
+                    //console.log(results);
+                    return results;
+                });
+
         }
     };
+
 
     return myService;
 
