17a06e7d54eae8bb32fa12d6705d29ce15949923
lalit <lalitisbhatia@yahoo.com>
2014-05-20 22:04:45 -0400

    Completed admin service cleanup

--- public/js/adminService.js
+++ public/js/adminService.js
@@ -1,82 +1,153 @@
 'use strict';
-adminModule.factory('adminService',function($http,$log,$q,$rootScope){
-    var deffered = $q.defer();
-    var promise;
-
-    var myService = {};
-    var data = [];
-
-
-    /**************************************************************************
-     ** User's pilot profile to be made available to all controllers
-     **************************************************************************/
-
-    myService.Coaches ; //we'll use this vale to share across the app among different controllers
-
-    //this is the method that sets the value of the coaches property and calls the method to do the broadcasting
-    myService.prepForBroadcast = function(coaches) {
-        this.Coaches = coaches;
-        this.broadcastItem();
-    };
-
-    //this is the method that broadcasts/publishes the event 'handleCoachesBroadcast'
-    myService.broadcastItem = function() {
-        $rootScope.$broadcast('handleCoachesBroadcast');
-    };
-
-
-    /*******************************************************
-     * Get all users for the pilot
-     * @type {{}}
-     */
-    var allUsers={};
-    myService.getAllUsers = function(){
-        if(!promise){
-            promise= $http({
-                method: 'GET',
-                url: '/users'
+adminModule.factory('testAdminService',function($http,$log,$q,$rootScope){
+    var myService = {
+        getAllUsers: function () {
+            return $http({
+                    method: 'GET',
+                    url: '/users'
                 })
-                .success(function(data) {
-                    $log.info("Successfully retrieved all users.");
-                    allUsers = data;
-                    deffered.resolve();
+                    .success(function (data) {
+                        $log.info("Successfully retrieved all users.");
+                    })
+                    .error(function (status, headers, config) {
+                        $log.log('failed to users: ' + status);
+                    })
+                    .then(function(response) {
+                        var results = [];
+                        results=response.data;
+                        //console.log(results);
+                        return results;
+                    });
+
+            },
+        addNewUser:function(user){
+            return $http({
+                method:'POST',
+                url: '/addUser',
+                data: user
+            })
+                .success(function (d, status, headers, config) {
+                    //console.log(d);
                 })
                 .error(function(status, headers, config){
-                    $log.log('failed to users: ' + status);
-                });
-            return deffered.promise;
-        } // closing brace for if(!promise)
-    };
+                    console.log('failed to save user:' + status);
+                })
+                .then(function(response){
+                    var results = [];
+                    results=response.data;
+                    //console.log(results);
+                    return results;
+                })
+        },
+        updateUser:function(user) {
+            return $http({
+                method: 'POST',
+                url: '/addUser',
+                data: user
+            })
+                .success(function (d, status, headers, config) {
+                    //console.log(d);
+                })
+                .error(function (status, headers, config) {
+                    console.log('failed to save user:' + status);
+                })
+                .then(function (response) {
+                    var results = [];
+                    results = response.data;
+                    //console.log(results);
+                    return results;
+                })
+        },
+        deleteUser:function(user){
+            $http({
+                method:'POST',
+                url: '/deleteUser',
+                data: user
+            })
+                .success(function (data, status, headers, config) {
+                    console.log('Successful call to db - ' + data);
+                    return data;
+                })
+                .error(function(status, headers, config){
+                    console.log('failed to delete coach:' + status);
+                })
 
-    myService.allUsers = function () {
-        return allUsers;
-    };
-    /*******************************************************
-     * Get all users for the pilot
-     * @type {{}}
-     */
-    var allCoaches={};
-    myService.getAllCoaches = function(){
-        if(!promise){
-            promise= $http({
+        },
+        getAllCoaches: function () {
+            return $http({
                 method: 'GET',
                 url: '/coaches'
+            })
+                .success(function (data) {
+                    $log.info("Successfully retrieved all coaches.");
                 })
-                .success(function(data) {
-                    $log.info("Successfully retrieved all users.");
-                    allCoaches = data;
-                    deffered.resolve();
+                .error(function (status, headers, config) {
+                    $log.log('failed to coaches: ' + status);
+                })
+                .then(function(response) {
+                    var results = [];
+                    results=response.data;
+                    //console.log(results);
+                    return results;
+                });
+
+        },
+        addNewCoach:function(coach){
+            return $http({
+                method:'POST',
+                url: '/addCoach',
+                data: coach
+            })
+                .success(function (d, status, headers, config) {
+                    //console.log(d);
                 })
                 .error(function(status, headers, config){
-                    $log.log('failed to users: ' + status);
-                });
-            return deffered.promise;
-        } // closing brace for if(!promise)
+                    console.log('failed to save coach:' + status);
+                })
+                .then(function(response){
+                    var results = [];
+                    results=response.data;
+                    //console.log(results);
+                    return results;
+                })
+        },
+        updateCoach:function(coach) {
+            return $http({
+                method: 'POST',
+                url: '/addCoach',
+                data: coach
+            })
+                .success(function (d, status, headers, config) {
+                    //console.log(d);
+                })
+                .error(function (status, headers, config) {
+                    console.log('failed to save coach:' + status);
+                })
+                .then(function (response) {
+                    var results = [];
+                    results = response.data;
+                    //console.log(results);
+                    return results;
+                })
+        },
+        deleteCoach:function(coach){
+            $http({
+                method:'POST',
+                url: '/deleteCoach',
+                data: coach
+            })
+                .success(function (data, status, headers, config) {
+                    console.log('Successful call to db - ' + data);
+                    return data;
+                })
+                .error(function(status, headers, config){
+                    console.log('failed to delete coach:' + status);
+                })
+
+        }
     };
 
-    myService.allCoaches = function () {
-        return allCoaches;
-    };
 
     return myService;
+
 });
